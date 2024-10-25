1b2006ff83cc51c25b9ddedee723136d7795a337
jareershafiq125 <jareershafiq22@gmail.com>
2024-10-25 06:49:41 +0500

    Add files via upload

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,22 @@
+
+import React from 'react'
+
+
+const page = () => {
+  return (
+    <><form >
+          <label>Enter your name:
+              <input type="text" />
+          </label>
+      </form>
+      <form>
+              <label>Enter your Email:
+                  <input type="text" />
+              </label>
+          </form>
+        
+          </>
+  )
+}
+
+export default page
